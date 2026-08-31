8aeb225026cb3fc6835c77c5feb2e1a8251ebc03
jda2011 <jdaadj0228@gmail.com>
2026-08-31 20:49:10 +0900

    Remove comments from next.config.js

--- app/app/app/app/next.config.js
+++ app/app/app/app/next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // 빌드 시 타입 에러가 있어도 무조건 통과시킵니다.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // 빌드 시 스타일/경고 에러가 있어도 무조건 통과시킵니다.
     ignoreDuringBuilds: true,
   },
 };
