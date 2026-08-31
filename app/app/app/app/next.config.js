7ca195bd834a7597c5965b7e20a04cecffca449d
jda2011 <jdaadj0228@gmail.com>
2026-08-31 20:46:14 +0900

    Update next.config.js

--- app/app/app/app/next.config.js
+++ app/app/app/app/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // 빌드 시 타입 에러가 있어도 무시하고 성공시킵니다.
+    // 빌드 시 타입 에러가 있어도 무조건 통과시킵니다.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // 빌드 시 코드 스타일 에러가 있어도 무시하고 성공시킵니다.
+    // 빌드 시 스타일/경고 에러가 있어도 무조건 통과시킵니다.
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
